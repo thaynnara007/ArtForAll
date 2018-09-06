3c600c94ff09f57a22a38cdd800afeb7710778ed
Thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2018-09-05 21:16:34 -0300

    define REST part2

--- app/index.js
+++ app/index.js
@@ -1,38 +1,81 @@
+
 const express = require('express');
 const app = express();
 
 app.listen(3000, () => console.log("app running at port 3000"));
 
-app.use(function (req, res, next) {
+app.use(function (req, res, next) { 
 
-	res.header('Content-Type', 'application/jason');
+	res.setHeader('Content-Type', 'application/json'); 
 	next();	
 });
 
-app.get('/profile', (req, res) =>{
+//----------------------INFO----------------------//
+var Info = function(name, old){
 
-	res.send(JSON.stringify({name:"Thaynnara", old:"21", following:40, id:1}))
+	this.name = name;
+	this.old = old;
+}
+
+var info1 = new Info("Thaynnara", 21);
+var info2 = new Info("Gabriel", 21);
+var info3 = new Info("Clara", 22);
+
+//---------------PROFILE-------------------------//
+var Profile = function(info, following, id){
+
+	this.info = info;
+	this.following = following;
+	this.id = id;
+}
+
+var person = new Profile(info1, 40, 1);
+var anotherPerson = new Profile(info2, 36, 2);
+var otherPerson =  new Profile(info3, 45, 3);
+
+profiles = [person, anotherPerson, otherPerson];
+
+//--------------------ART--------------------------//
+var Art = function(imgLink, tags, id){
+
+	this.imgLink = imgLink;
+	this.tags = tags;
+	this.id = id;
+}
+art1 = new Art('https://cdn.shopify.com/s/files/1/0558/2081/products/ATCAWM_FC_1024x1024.jpg?v=1534804643',
+		['adventure time', 'fanart', 'finn', 'jake'], 1);
+
+art2 = new Art('https://cdn.vox-cdn.com/thumbor/q0ujcr0H33ybBRUUNNFL1QVBAKY=/0x0:1920x1080/1200x800/filters:focal(760x282:1066x588)/cdn.vox-cdn.com/uploads/chorus_image/image/61137005/adventure_time_stakes.0.jpg',
+		['adventure time', 'marceline', 'bonnibel'], 2)
+
+arts = [art1, art2];
+
+//----------------------------------------------------//
+
+app.get('/profile', (req, res) =>{ 
+
+	res.json(profiles);
 });
 
 app.get('/profile/arts', (req, res) =>{
 
-	res.send(JSON.stringify({img:'https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwigw-P_yJ3dAhXBkpAKHZidABAQjRx6BAgBEAU&url=https%3A%2F%2Fwww.kidrobot.com%2Fcollections%2Fadventure-time-x-kidrobot&psig=AOvVaw20QPijfG3CRmcB-16ID32N&ust=1536020651358193',
-							tags:['adventure time', 'fanart', 'chibi'],
-							id:1,
-							img:"sdasdad",
-							tags:["boku no hero", 'original'],
-							id:2}));
+	res.json([arts]);
 });
 
-app.get('/profile/arts/{id}', (req, res) =>{
+app.get('/profile/arts/:id', (req, res) =>{ 
 
-		res.send(JSON.stringify({img:'https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwigw-P_yJ3dAhXBkpAKHZidABAQjRx6BAgBEAU&url=https%3A%2F%2Fwww.kidrobot.com%2Fcollections%2Fadventure-time-x-kidrobot&psig=AOvVaw20QPijfG3CRmcB-16ID32N&ust=1536020651358193',
-							tags:['adventure time', 'fanart', 'chibi'],
-							id:1}));
+	var id = req.params.id
+	res.json(arts[id]);
 });
 
 app.get('/profile/following', (req, res) =>{
 
-	res.send(JSON.stringify({name:"gabriel",
-							name:"clara"}));
+	names = ["clara", "gabriel", "igor", "sophia"];
+	res.json(names);
 });
+
+app.get('/profile/following/:id', (req, res) =>{
+
+	var id = req.params.id;
+	res.json(profiles[id]);
+})
